b6e28f65c7b9888222488e4ad87f9d64d2dcfd5d
baroudeColombbus <barry-ntsiba@protonmail.com>
2021-09-20 15:30:51 +0100

    follow up

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CSSVarsApp from './css-vars'
+
+
+
+function App() {
+  return (
+    <div>
+      <div>
+
+      </div>
+      <CSSVarsApp />
+    </div>
+  )
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
